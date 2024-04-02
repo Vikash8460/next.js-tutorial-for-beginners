6d65acb419ef6a5888b2b960634baa7d3a7d0a92
Vikash8460 <Vikashkamat8080@gmail.com>
2024-04-02 13:50:46 +0530

    changing in the page.tsx

--- next.js/tutorial2/src/app/page.tsx
+++ next.js/tutorial2/src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Hello Vikash</h1>
+      <Link href="/about">Go to About Page</Link>
     </main>
   )
 }
